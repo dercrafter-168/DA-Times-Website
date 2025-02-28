1e75689139e711c6bb54a7951f9e71bfa686a285
dercrafter_168 <mike@der-crafter.de>
2025-02-28 22:41:17 +0100

    [1.0.0.4] added some new features - added default.js - started with navbar.js

--- data/js/navbar.js
+++ data/js/navbar.js
@@ -1,13 +1,37 @@
-// Warten, bis das Dokument geladen ist
+console.log("[DCN-System] navbar.js is loading");
 document.addEventListener('DOMContentLoaded', function() {
-    // Button-Element auswählen
-    const nav_button_1 = document.getElementById('navbar-item-1');
 
-    // Klick-Event hinzufügen
+    const nav_button_1 = document.getElementById('navbar-item-1');
+    const nav_button_2 = document.getElementById('navbar-item-2');
+    const nav_button_3 = document.getElementById('navbar-item-3');
+    const nav_button_4 = document.getElementById('navbar-item-4');
+    const drp_nav_button_1 = document.getElementById('navbar-item-drp-1');
+    const drp_nav_button_2 = document.getElementById('navbar-item-drp-2');
+
     nav_button_1.addEventListener('click', function() {
-        alert('Hallo!');
+        window.location.replace("./index.html");
+        console.log("Button 1 clicked & user redirected to index.html");
+    });
+
+    nav_button_2.addEventListener('click', function() {
         window.location.replace("https://beispiel.de");
     });
 
+    nav_button_3.addEventListener('click', function() {
+        window.location.replace("https://beispiel.de");
+    });
+
+    nav_button_4.addEventListener('click', function() {
+        window.location.replace("https://beispiel.de");
+    });
+
+    drp_nav_button_1.addEventListener('click', function() {
+        window.location.replace("https://beispiel.de");
+    });
+
+    drp_nav_button_2.addEventListener('click', function() {
+        window.location.replace("https://beispiel.de");
+    });
 
 });
+console.log("[DCN-System] navbar.js is loaded");
